ab5662cd131c41729c3b469fa8df27174499b9f1
ManDuong25 <90708115+ManDuong25@users.noreply.github.com>
2024-10-13 15:33:01 +0700

    khung layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
+import { HeaderOnly } from '~/components/Layouts';
 import HomePage from '~/pages/Home';
 import FollowingPage from '~/pages/Following';
+import UploadPage from '~/pages/Upload';
+import SearchPage from '~/pages/Search';
 
 const publicRoutes = [
     {
@@ -9,6 +12,16 @@ const publicRoutes = [
     {
         path: '/following',
         component: FollowingPage,
+    },
+    {
+        path: '/upload',
+        component: UploadPage,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: SearchPage,
+        layout: null,
     },
 ];
 
